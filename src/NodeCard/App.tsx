71abfa795c4fb435ceed5e0fb80226b8462d59c0
RandonSRProctor <RandyProctor@Gmail.com>
2023-09-15 23:01:19 -0400

    App is dead.  Long live App.

---
+++ src/NodeCard/App.tsx
@@ -0,0 +1,10 @@
+import JSONVisualizer from '../JSONVisualizer';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <JSONVisualizer />;
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import JSONVisualizer from './JSONVisualizer';
+import { App } from './NodeCard/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <JSONVisualizer />
+    <App />
   </React.StrictMode>
 );
